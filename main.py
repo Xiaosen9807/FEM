e3bdbad071d9a6dbf507ea1ea851a82009d615c2
Xusen Qin <184685245@qq.com>
2022-09-19 23:10:35 +0200

    xxx

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import torch
+from torch import atan
+from torch.autograd import grad
 import numpy as np
+import matplotlib.pyplot as pltcon
+from scipy.interpolate import lagrange
+from scipy.misc import derivative
+
+from sympy import symbols, integrate, sinh, E, diff
+import sympy as sp
+from scipy.special import roots_legendre
 
 print('hello')
